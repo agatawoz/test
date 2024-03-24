1991c4134f2b3f87c404bdda69f1dc73053bec2c
Hryhorii Shevchenko <hryhoriishevchenko.mail@gmail.com>
2024-03-24 16:37:37 +0100

    added GridPane and some buttons

--- src/main/java/com/example/java_test/HelloApplication.java
+++ src/main/java/com/example/java_test/HelloApplication.java
@@ -2,7 +2,9 @@ package com.example.java_test;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,10 +12,12 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("scene1.fxml"));
+        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+        Scene scene = new Scene(fxmlLoader.load(), screenBounds.getWidth(), screenBounds.getHeight()-30);
+        stage.setTitle("Test window");
         stage.setScene(scene);
+        stage.setMaximized(true);
         stage.show();
     }
 
