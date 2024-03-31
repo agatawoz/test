e6a79dc881ad5388f1b4a919b0c6311e986efeba
Hryhorii Shevchenko <hryhoriishevchenko.mail@gmail.com>
2024-03-31 15:42:40 +0200

    dodane przyciski (kalendarz)

--- src/main/java/com/example/java_test/HelloApplication.java
+++ src/main/java/com/example/java_test/HelloApplication.java
@@ -6,6 +6,7 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -15,8 +16,10 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("stronaGlowna_v2.fxml"));
         Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
         Scene scene = new Scene(fxmlLoader.load(), screenBounds.getWidth(), screenBounds.getHeight()-30);
-        stage.setTitle("Test window");
         stage.setScene(scene);
+        String css = HelloApplication.class.getResource("style.css").toExternalForm();
+        scene.getStylesheets().add(css);
+        stage.setTitle("Test window");
         stage.setMaximized(true);
         stage.show();
     }
